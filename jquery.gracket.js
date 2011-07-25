18e3b0a8888e321e6a7f165463c2eb8e1771da85
unknown <Erik Zettersten@.(none)>
2011-07-25 19:53:55 -0400

    first round commits

---
+++ jquery.gracket.js
@@ -0,0 +1,58 @@
+// ItsGosu | Bracket Plugin (jquery.gracket.js)
+// Erik Zettersten
+// Version 0.1
+
+(function($) {
+    $.fn.gracket = function(method) {
+
+        // Public methods
+        var methods = {
+            init : function(options) {
+                this.gracket.settings = $.extend({}, this.gracket.defaults, options);
+				alert(options.foo);
+                return this.each(function() {
+					console.log(options.foo);
+                });
+            },
+
+			// We can call methods publically
+			// $("[data-gracket]").gracket('foo_public_method');
+            foo_public_method: function() {
+               alert("public method");
+            }
+
+        }
+
+        // Private methods
+        var helpers = {
+			create_tier : function(){
+				alert("create tier");
+			},
+			create_node : function(){
+				
+			}
+        }
+
+        // if a method as the given argument exists
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error( 'Method "' +  method + '" does not exist in gracket!');
+        }
+
+    }
+
+    // Defaults
+    $.fn.gracket.defaults = {}
+
+	// Defaults => Settings
+    $.fn.gracket.settings = {}
+
+})(jQuery);
+
+// Call Plugin
+$("[data-gracket]").gracket({
+	foo : "bar"
+});
