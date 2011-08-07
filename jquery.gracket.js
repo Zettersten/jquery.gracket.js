ac562e4a557197ccdabe83f0d9146ee728e3e2f2
Erik Zettersten <erik@zettersten.com>
2011-08-07 00:54:31 -0400

    Updated to Ver 0.2!
    => Got the dang math to work… => Considering add spacer logic - seems
    really complex

--- jquery.gracket.js
+++ jquery.gracket.js
@@ -1,6 +1,6 @@
 // ItsGosu | Bracket Plugin | Gracket (jquery.gracket.js)
 // Erik Zettersten
-// Version 0.1
+// Version 0.2
 
 (function($) {
 	$.fn.gracket = function(method) {
@@ -10,70 +10,121 @@
 		var team_count;
 		var round_count;
 		var game_count;
+		
+		
+		// Defaults
+		$.fn.gracket.defaults = {
+			gameClass : "g_game",
+			roundClass : "g_round",
+			teamClass : "g_team",
+			nodeWidth : 120,
+			winnerClass : "g_winner",
+			connectorClass : "g_connector"
+		}
+		
+		// Defaults => Settings
+		$.fn.gracket.settings = {}
 
 		// Public methods
 		var methods = {
 			init : function(options) {
+				
 				this.gracket.settings = $.extend({}, this.gracket.defaults, options);
 		
+				// get data length
 				round_count = data.length;
-		
+				
 				//  create rounds
 				for (var r=0; r < round_count; r++) {
-		
-					var round_html = helpers.build.round();
+					
+					var round_html = helpers.build.round(this.gracket.settings);
 					container.append(round_html);
 		
 					// create games in round
-					game_count = data[r].length;
-		
+					game_count = data[r].length;		
 					for (var g=0; g < game_count; g++) {
 		
-						var game_html = helpers.build.game();
+						var game_html = helpers.build.game(this.gracket.settings);
 						round_html.append(game_html);
-		
+						
+						// align game
+						helpers.align.game(game_html, container.find("." + this.gracket.settings.gameClass).outerHeight(true), r);
+						
 						// create teams in game
 						team_count = data[r][g].length;
-		
 						for (var t=0; t < team_count; t++) {
 		
-							var team_html = helpers.build.team();
+							var team_html = helpers.build.team(data[r][g][t], this.gracket.settings);
 							game_html.append(team_html);
+							
+							// adjust winner
+							if (team_count === 1) {
+								helpers.align.winner(game_html, this.gracket.settings, container.find("." + this.gracket.settings.gameClass).outerHeight(true), r);
+								
+								// init the listeners after gracket is built
+								helpers.listeners();
+							}
 		
 						};
 		
 					};
-		
+					
 				};
+				
 		
 			}
+		
 		};
+
+		
 
 
 		
 		// Private methods
 		var helpers = {
 			build : {
-				team : function(){
+				team : function(data, node){
 					return team = $("<div />", {
-						html : "<h3><span>{{id}}</span>{{name}}</h3>",
-						class : "{{class}} g_team"
+						html : "<h3><span>"+ (data.id || 0) +"</span>"+ data.name +"</h3>",
+						class : node.teamClass + " " + data.id
 					});
 				},
-				game : function(){
+				game : function(node){
 					return game = $("<div />", {
-						class : "{{class}} g_game"
+						class : node.gameClass,
+						html : "<div class="+ node.connectorClass +"></div>"
 					}).css({
-						width : 120
+						width : node.nodeWidth
 					});
 				},
-				round : function(){
+				round : function(node){
 					return round = $("<div />", {
-						class : "{{class}} g_round"
+						class : node.roundClass
 					}).css({
-						width : 120
+						width : node.nodeWidth
 					});
 				}
+			},
+			align : {
+				game : function(game_html, yOffset, r){
+					// only get the top of each game so we can move accordingly
+					if (game_html.parent().index() > 0 && game_html.index() === 0) {
+						return game_html.css({ marginTop: yOffset + (Math.pow(2, (r - 2)) * yOffset - (yOffset / 2)) });
+					};
+				},
+				winner : function(game_html, node, yOffset, r){
+					return game_html.addClass(node.winnerClass).css({ height : game_html.height() * 2, marginTop: yOffset + (Math.pow(2, (r - 3)) * yOffset - (yOffset / 2)) });
+				}
+			}, 
+			listeners : function(){	
+				
+				// example only
+				$(".g_1234").hover(function(){
+					$(".g_1234").css({ background : "yellow" })
+				}, function(){
+					$(".g_1234").css({ background : "none" })
+				});
+				
 			}
 		};
 	
@@ -87,11 +138,6 @@
 		}
 	
 	}
-	
-	// Defaults
-	$.fn.gracket.defaults = {}
-	// Defaults => Settings
-	$.fn.gracket.settings = {}
 
 })(jQuery);
 
