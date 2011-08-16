9a4f310888d54cb41a10e53c940f9198bf2aa3d7
Erik Zettersten <erik@zettersten.com>
2011-08-16 16:32:12 -0400

    CSS refactor

--- examples/gosu/config.rb
+++ examples/gosu/config.rb
@@ -8,13 +8,13 @@ images_dir = "css/images"
 javascripts_dir = ""
 
 # You can select your preferred output style here (can be overridden via the command line):
-# output_style = :expanded or :nested or :compact or :compressed
+output_style = :compact # or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
